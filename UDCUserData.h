b57e9ed0019c902d39585c25be242f0a807073f5
Jose Manuel Ramírez Martínez <jmramirez@motofan.com>
2014-09-15 20:25:44 +0200

    finished

---
+++ UDCUserData.h
@@ -0,0 +1,13 @@
+//
+//  UDCUserData.h
+//  User Data Challenge
+//
+//  Created by Jose Manuel Ramirez Martinez on 15/09/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UDCUserData : NSObject
+
+@end
---
+++ UDCUserData.m
@@ -0,0 +1,13 @@
+//
+//  UDCUserData.m
+//  User Data Challenge
+//
+//  Created by Jose Manuel Ramirez Martinez on 15/09/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import "UDCUserData.h"
+
+@implementation UDCUserData
+
+@end
